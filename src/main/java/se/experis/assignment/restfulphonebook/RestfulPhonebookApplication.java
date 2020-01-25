12b48151f39600341bff3e44475050a4c67b1ab0
Malek Al-Harani <malek.horani432@hotmail.com>
2020-01-25 12:59:46 +0100

    Setting up The project

---
+++ src/main/java/se/experis/assignment/restfulphonebook/RestfulPhonebookApplication.java
@@ -0,0 +1,13 @@
+package se.experis.assignment.restfulphonebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulPhonebookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulPhonebookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/experis/assignment/restfulphonebook/RestfulPhonebookApplicationTests.java
@@ -0,0 +1,13 @@
+package se.experis.assignment.restfulphonebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulPhonebookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
